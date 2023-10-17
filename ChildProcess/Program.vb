23aa7d59d50d3a9807bf270cd1d6547d379401f4
Joseph Guenther <joseph@codingcool.ca>
2023-10-16 21:02:06 -0400

    Add headless mode

---
+++ ChildProcess/Program.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Text
+
+Public Module Program
+    ReadOnly _client As New PipeClient
+    Dim _id As String
+
+    Public Sub Main(args As String())
+        _id = args(0)
+        _client.Connect("\\.\pipe\TestMaster1")
+        AddHandler _client.MessageReceived, AddressOf MessageRecieved
+        Console.ReadKey()
+    End Sub
+
+    Private Async Sub MessageRecieved(message() As Byte)
+        Dim cmd As String = Encoding.UTF8.GetString(message)
+        Console.WriteLine($"Received: {cmd}")
+        If cmd = $"Close:{_id}" Then
+            CloseSafe()
+        ElseIf cmd = $"Message:{_id}" Then
+            _client.SendMessage(Encoding.UTF8.GetBytes("Hi"))
+        End If
+    End Sub
+
+    Private Sub CloseSafe()
+        _client.Disconnect()
+        End
+    End Sub
+End Module
--- TabbedMultiProcess/frmMain.vb
+++ TabbedMultiProcess/frmMain.vb
@@ -3,7 +3,7 @@ Imports System.Text
 Imports System.Threading
 
 Public Class frmMain
-    Dim children As New Dictionary(Of TabPage, Process)
+    Dim children As New Dictionary(Of TabPage, (Process, Integer))
     Dim server As New PipeServer
 
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -16,7 +16,7 @@ Public Class frmMain
         If s.Contains("Closing") Then
             Dim handle As New IntPtr(CInt(s.Split(":")(1)))
             For Each t As TabPage In tcApps.TabPages
-                If children(t).MainWindowHandle = handle Then
+                If children(t).Item2 = handle Then
                     Invoke(Sub() tcApps.TabPages.Remove(t))
                     Exit For
                 End If
@@ -27,52 +27,32 @@ Public Class frmMain
     End Sub
 
     Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
-        Dim p As Process = Process.Start("C:\CodingCool\Code\Projects\TabbedMultiProcess\ChildProcess\bin\Debug\net7.0-windows\ChildProcess.exe")
-        p.WaitForInputIdle()
+        Dim id As Integer = Random.Shared.Next
+        Dim p As Process = Process.Start("C:\CodingCool\Code\Projects\TabbedMultiProcess\ChildProcess\bin\Debug\net7.0\ChildProcess.exe", id)
         p.EnableRaisingEvents = True
         AddHandler p.Exited, AddressOf p_Exited
         Dim t As New TabPage(p.MainWindowTitle)
+        Dim button As New Button With {.Text = "Send Message", .Dock = DockStyle.Top}
+        AddHandler button.Click, Sub()
+                                     server.SendMessage(Encoding.UTF8.GetBytes($"Message:{id}"))
+                                 End Sub
+        t.Controls.Add(button)
         tcApps.TabPages.Add(t)
-        While WindowsAPI.SetParent(p.MainWindowHandle, t.Handle) = IntPtr.Zero
-            Thread.Yield()
-        End While
-        WindowsAPI.ShowWindow(p.MainWindowHandle, WindowsConstants.SW_MAXIMIZE)
-        SetBorderStyle(p)
-        children.Add(t, p)
+        children.Add(t, (p, id))
     End Sub
 
     Private Sub p_Exited(sender As Object, e As EventArgs)
         For Each t As TabPage In tcApps.TabPages
-            If children(t) Is sender Then
-                tcApps.TabPages.Remove(t)
+            If children(t).Item1 Is sender Then
+                Invoke(Sub() tcApps.TabPages.Remove(t))
                 Exit For
             End If
         Next
     End Sub
 
-    Private Sub SetBorderStyle(p As Process)
-        Dim style As Long = WindowsAPI.GetWindowLongPtr(New HandleRef(Me, p.MainWindowHandle), WindowsConstants.GWL_STYLE).ToInt64()
-        style = style And Not (WindowsConstants.WS_CAPTION Or WindowsConstants.WS_BORDER Or WindowsConstants.WS_DLGFRAME)
-        Dim styleValue As New IntPtr(style)
-        WindowsAPI.SetWindowLongPtr(New HandleRef(Me, p.MainWindowHandle), WindowsConstants.GWL_STYLE, styleValue)
-        style = WindowsAPI.GetWindowLongPtr(New HandleRef(Me, p.MainWindowHandle), WindowsConstants.GWL_STYLE)
-        style = style And Not WindowsConstants.WS_POPUP
-        style = style Or WindowsConstants.WS_CHILD
-        styleValue = New IntPtr(style)
-        WindowsAPI.SetWindowLongPtr(New HandleRef(Me, p.MainWindowHandle), WindowsConstants.GWL_STYLE, styleValue)
-    End Sub
-
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
-        server.SendMessage(Encoding.UTF8.GetBytes($"Close:{children(tcApps.SelectedTab).MainWindowHandle}"))
+        server.SendMessage(Encoding.UTF8.GetBytes($"Close:{children(tcApps.SelectedTab).Item2}"))
         tcApps.TabPages.Remove(tcApps.SelectedTab)
-    End Sub
-
-    Private Sub tcApps_SizeChanged(sender As Object, e As EventArgs) Handles tcApps.SizeChanged
-        For Each t As TabPage In tcApps.TabPages
-            Dim p As Process = children(t)
-            WindowsAPI.MoveWindow(p.MainWindowHandle, 0, 0, t.Width, t.Height, True)
-            SetBorderStyle(p)
-        Next
     End Sub
 
     Private Sub btnBye_Click(sender As Object, e As EventArgs) Handles btnBye.Click
